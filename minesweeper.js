c4460011ebc44d0476b1a3961c0094ff6c2a38c9
miro5460 <robin411989@gmail.com>
2017-08-23 16:30:23 -0400

    Add files via upload

---
+++ minesweeper.js
@@ -0,0 +1,108 @@
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++){
+    let row = [];
+    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return (board);
+}; // function generates game board with unselected tiles for player
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++){
+    let row = [];
+    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
+  let numberOfBombsPlaced = 0;
+  while (numberOfBombsPlaced < numberOfBombs) {
+    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    if (board[randomRowIndex][randomColumnIndex] !== 'B') {
+      board[randomRowIndex][randomColumnIndex] = 'B';
+      numberOfBombsPlaced++;
+    };
+  }
+  return board;
+}; // function generates bomb board with randomly placed bombs
+
+const getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
+  const neighborOffets = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]];
+  const numberOfRows = bombBoard.length;
+  const numberOfColumns = bombBoard[0].length;
+  let numberOfBombs = 0;
+  neighborOffets.forEach(offset => {
+    const neighborRowIndex = rowIndex + offset[0];
+    const neighborColumnIndex = columnIndex + offset[1];
+    if (neighborRowIndex >= 0 && neighborRowIndex < numberOfRows && neighborColumnIndex >= 0 &&
+        neighborColumnIndex <numberOfColumns) {
+        if  (bombBoard[neighborRowIndex][neighborColumnIndex] === 'B') {
+          numberOfBombs++;
+        }
+    }
+  });
+  return numberOfBombs;
+}; // function checks for bombs in neighboring tiles and returns that number
+
+const flipTile = (playerBoard, bombBoard, rowIndex, columnIndex) => {
+  if (playerBoard[rowIndex][columnIndex] !== ' ') {
+     console.log('This tile has already been flipped!');
+     return
+  } else if (bombBoard[rowIndex][columnIndex] === 'B') {
+    playerBoard[rowIndex][columnIndex] = 'B';
+  } else {
+    playerBoard[rowIndex][columnIndex] = getNumberOfNeighborBombs(bombBoard, rowIndex, columnIndex);
+  };
+}; // function allows player to select tiles on game board, and compares the bomb and player boards
+
+const printBoard = board => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+};
+
+let playerBoard = generatePlayerBoard(3,4); // specifies size of player board
+let bombBoard = generateBombBoard(3,4,5);  // specifies size of bomb board (should match player board dimensions) and number of bombs
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
+flipTile(playerBoard, bombBoard, 0, 0); // specifies player tile selection
+console.log('Updated Player Board: ');
+printBoard(playerBoard);
+
+
+/* const printBoard = (board) => {
+  console.log('Current Board: ')
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+}; // displays current board */
+
+/* const blankLine = '  |   |  ';
+console.log('This is what an empty board would look like:');
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine); // creates empty board
+
+const guessLine = '1 |   |  ';
+const bombLine =  '  | B | ';
+
+console.log('This is what a board with a guess and a bomb on it would look like:');
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine); // creates populated board */
+
+/* let board = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' ']]; // creates board from array
+
+
+printBoard(board); // prints array board to console
+board[0][1] = '1';
+board[2][2] = 'B';
+printBoard(board); */
